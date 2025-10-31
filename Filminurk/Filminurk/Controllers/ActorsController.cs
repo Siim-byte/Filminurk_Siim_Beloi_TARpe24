49655cbf5301ffacdbd5e06916e93be839010076
Opilane TTHK <189960872+Siim-byte@users.noreply.github.com>
2025-10-31 12:31:41 +0200

    Added actorsservicces interface added actores createupdate view and model added actorsdto added  method in controller for create

--- Filminurk/Filminurk/Controllers/ActorsController.cs
+++ Filminurk/Filminurk/Controllers/ActorsController.cs
@@ -1,4 +1,6 @@
-﻿using Filminurk.Data;
+﻿using Filminurk.Core.Dto;
+using Filminurk.Core.ServiceInterface;
+using Filminurk.Data;
 using Filminurk.Models.Actors;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,10 +9,16 @@ namespace Filminurk.Controllers
     public class ActorsController : Controller
     {
         private readonly FilminurkTARpe24Context _context;
+        private readonly IActorsServices _actorsServices;
         
-        public ActorsController(FilminurkTARpe24Context context)
+        public ActorsController
+            (
+            FilminurkTARpe24Context context,
+            IActorsServices actorsServices
+            )
         {
             _context = context;
+            _actorsServices = actorsServices;
         }
         public IActionResult Index()
         {
@@ -24,6 +32,46 @@ namespace Filminurk.Controllers
 
             });
             return View(result);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ActorsCreateUpdateViewModel result = new();
+            return View("CreateUpdate", result);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(ActorsCreateUpdateViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var dto = new ActorsDTO()
+                {
+                    ActorID = (Guid)vm.ActorID,
+                    FirstName = vm.FirstName,
+                    LastName = vm.LastName,
+                    NickName= vm.NickName,
+                    MoviesActedFor = vm.MoviesActedFor,
+                    PortraitID = vm.PortraitID,
+                    HomeCountry = vm.HomeCountry,
+                    HomeCity = vm.HomeCity,
+                    HomeRegion = vm.HomeRegion,
+                    EntryCreatedAt = vm.EntryCreatedAt,
+                    EntryModifiedAt = vm.EntryModifiedAt
+
+                };
+                var result = await _actorsServices.Create(dto);
+                if (result == null)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public async Task<IActionResult> Update(Guid id)
+        {
+            var actors = await _actorsServices.
         }
     }
 }
---
+++ Filminurk/Filminurk/Models/Actors/ActorsDetailsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Filminurk.Core.Domain;
+
+namespace Filminurk.Models.Actors
+{
+    public class ActorsDetailsViewModel
+    {
+        public Guid? ActorID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        public List<string> MoviesActedFor { get; set; }
+        public int PortraitID { get; set; }
+
+        //andmebaasi jaoks vajalik
+
+        public DateTime? EntryCreatedAt { get; set; }
+        public DateTime? EntryModifiedAt { get; set; }
+
+        //minu mõeldud
+
+        public HomeCountry? HomeCountry { get; set; }
+        public string? HomeCity { get; set; }
+        public string? HomeRegion { get; set; }
+    }
+}
+
