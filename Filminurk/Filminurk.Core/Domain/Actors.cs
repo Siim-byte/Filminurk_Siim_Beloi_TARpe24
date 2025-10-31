06046849e497544bbf757e1c19ce1a332070ac2f
Opilane TTHK <189960872+Siim-byte@users.noreply.github.com>
2025-10-31 11:25:26 +0200

    Made Actors class into Domain and filled it with required datatypes alse made a class for enum

---
+++ Filminurk/Filminurk.Core/Domain/Actors.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filminurk.Core.Domain
+{
+    public class Actors
+    {
+        public Guid ActorID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        public List<string> MoviesActedFor { get; set; }
+        public int PortraitID { get; set; }
+
+        //andmebaasi jaoks vajalik
+
+        public DateTime? EntryCreatedAt { get; set; }
+        public DateTime? EntryModifiedAt { get; set; }
+
+        //minu mõeldud
+
+        public HomeCountry? HomeCountry { get; set; }
+        public string? HomeCity { get; set; }
+        public string? HomeRegion { get; set; }
+    }
+}
---
+++ Filminurk/Filminurk.Core/Domain/HomeCountry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filminurk.Core.Domain
+{
+    public enum HomeCountry
+    {
+        England,
+        Usa,
+        French,
+        Poland,
+        Asia,
+        Korea,
+        China,
+        Mexico
+    }
+}
