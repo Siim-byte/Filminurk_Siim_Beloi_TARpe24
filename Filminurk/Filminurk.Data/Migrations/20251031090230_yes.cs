e04c560af45f15163958f8e59b19d3dec504898f
Opilane TTHK <189960872+Siim-byte@users.noreply.github.com>
2025-10-31 11:13:58 +0200

    clear

---
+++ Filminurk/Filminurk.Data/Migrations/20251031090230_yes.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Filminurk.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class yes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserComments",
+                columns: table => new
+                {
+                    CommentID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CommenterUserID = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CommentBody = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CommentedScore = table.Column<int>(type: "int", nullable: false),
+                    IsHelpful = table.Column<int>(type: "int", nullable: false),
+                    IsHarmful = table.Column<int>(type: "int", nullable: false),
+                    CommentCreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CommentModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CommentDeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    MovieID = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserComments", x => x.CommentID);
+                    table.ForeignKey(
+                        name: "FK_UserComments_Movies_MovieID",
+                        column: x => x.MovieID,
+                        principalTable: "Movies",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserComments_MovieID",
+                table: "UserComments",
+                column: "MovieID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserComments");
+        }
+    }
+}
