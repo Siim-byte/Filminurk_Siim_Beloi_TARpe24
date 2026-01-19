e449a3861a906f77356eacb8fb5815a8c79ccb95
Opilane TTHK <189960872+Siim-byte@users.noreply.github.com>
2026-01-19 10:21:53 +0200

    OMDbService not finished

--- Filminurk/Filminurk.ApplicationServices/Services/OMDbService.cs
+++ Filminurk/Filminurk.ApplicationServices/Services/OMDbService.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Filminurk.ApplicationServices.Services
@@ -18,6 +19,31 @@ namespace Filminurk.ApplicationServices.Services
 
             var baseUrl = "http://www.omdbapi.com/?apikey=[7dbe858b]&";
             var omdbUrl = $"http://img.omdbapi.com/?apikey=[7dbe858b]&";
+
+           /* using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri(omdbUrl);
+                httpClient.DefaultRequestHeaders.Accept.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(
+                    new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json")
+                );
+                var response = httpClient.GetAsync($"?q={dto.Title}&apikey={apikey}&details=true").GetAwaiter().GetResult();
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                try
+                {
+                    List<OMDbApiRootDTO> omdbData = JsonSerializer.Deserialize<List<OMDbApiRootDTO>>(jsonResponse);
+                    //dto.Title = omdbData[0].
+                    //dto.Year = omdbData[0].
+                }
+            }
+            string omdbResponse = baseUrl + $"{dto.Year}?apikey={apikey}&metric=true";
+            using (var clientOMDb = new HttpClient())
+            {
+                var httpResponseOMDb = clientOMDb.GetAsync(omdbResponse).GetAwaiter().GetResult();
+                string jsonOMDb = await httpResponseOMDb.Content.ReadAsStringAsync();
+
+
+            }*/
         }
     }
 }
